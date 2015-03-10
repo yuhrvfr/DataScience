595676870b2eb17726df567fff3f0adf0590de9d
Herve Yu <yuhrvfr@gmail.com>
2015-03-09 22:07:17 -0700

    Update server.R

--- DataProduct/server.R
+++ DataProduct/server.R
@@ -6,7 +6,7 @@ fit <- lm(mpg~factor(am)+wt+hp+cyl+disp+drat+qsec+carb+vs,data=mtcars)
 sfit <- step(fit,trace=0)
 
 drawPlots <- function(x) {
-		   par(mfrow=c(1,2))
+           par(mfrow=c(1,2))
      	   plot(mpg~wt,pch=19, xlab="Car weight (1000lbs)", col="darkgrey",
                 main="MPG over Car Weight")
            lmwt <- lm(mpg~wt)
@@ -15,20 +15,20 @@ drawPlots <- function(x) {
 
            plot(mpg~qsec,pch=19, xlab="Quarter Mile Second", col="darkgrey",
                 main="MPG over Quarter Miles Second")
-                lmqsec <- lm(mpg~qsec)
-                abline(lmqsec,col="darkgrey",lwd=2)
-	       points(x[2], jitter(x[3]), col="darkblue", pch=17, cex=2)
+           lmqsec <- lm(mpg~qsec)
+           abline(lmqsec,col="darkgrey",lwd=2)
+	   points(x[2], jitter(x[3]), col="darkblue", pch=17, cex=2)
 }
 
 shinyServer(
 
     function(input, output) {
-		output$mpgwt <- renderPlot({
-		     e <- data.frame(am=input$am,wt=input$wt,qsec=input$qsec)
-             p <- predict(sfit,e)
-		     output$empg <- renderPrint({as.character(p)})
-		     x <- c(input$wt,input$qsec,p)
-		     drawPlots(x)
-        })
-	}
+             output$mpgwt <- renderPlot({
+	           e <- data.frame(am=input$am,wt=input$wt,qsec=input$qsec)
+                   p <- predict(sfit,e)
+                   output$empg <- renderPrint({as.character(p)})
+                   x <- c(input$wt,input$qsec,p)
+	           drawPlots(x)
+             })
+        }
 )
